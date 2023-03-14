51373a815cc205eb5684c5f249923425e88b4784
MarkusAnders <ser.play02@mail.ru>
2023-03-14 14:33:51 +0300

    Добавьте файлы проекта.

---
+++ Models/Client.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
+
+namespace DentalCenter.Models
+{
+	public class Client
+	{
+		public int ClientId { get; set; }
+		[Display(Name = "Фамилия")]
+		public string ClientSurname { get; set; }
+		[Display(Name = "Имя")]
+		public string ClientName { get; set; }
+		[Display(Name = "Отчество")]
+		public string ClientPatronymic { get; set; }
+		[Display(Name = "Номер телефона")]
+		public int ClientPhone { get; set; }
+
+	}
+}
---
+++ Models/DentalCenterDBContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DentalCenter.Models
+{
+    public class DentalCenterDBContext : DbContext
+    {
+        public DentalCenterDBContext(DbContextOptions<DentalCenterDBContext> options) : base(options) { }
+        
+        public DbSet<Client> Clients { get; set; }  
+    }
+}
