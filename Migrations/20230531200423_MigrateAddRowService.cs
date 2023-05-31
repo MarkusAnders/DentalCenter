e90db5c51efb691eb6b5b2996685e6f2c27b90b0
MarkusAnders <ser.play02@mail.ru>
2023-05-31 23:13:34 +0300

    Добавление поля

---
+++ Migrations/20230531200423_MigrateAddRowService.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DentalCenter.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigrateAddRowService : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ServiceDecription",
+                table: "Service",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ContactMessage",
+                table: "Contact",
+                type: "nvarchar(300)",
+                maxLength: 300,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ServiceDecription",
+                table: "Service");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ContactMessage",
+                table: "Contact",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(300)",
+                oldMaxLength: 300);
+        }
+    }
+}
--- Models/Service.cs
+++ Models/Service.cs
@@ -7,7 +7,13 @@ namespace DentalCenter.Models
         public int ServiceId { get; set; }
         [Display(Name = "Название услуги")]
         public string ServiceName { get; set; }
+
+        [StringLength(150)]
+        [Display(Name = "Описание услуги")]
+        public int ServiceDecription { get; set; }
+
         [Display(Name = "Цена услуги")]
         public int ServicePrice { get; set; }
+        
     }
 }
